ef76cc9578050d830d9c6df7094704269c009ab0
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2020-01-08 19:16:08 +0700

    Fixing Load Scene
    nambah gameobject jack

---
+++ Assets/Scripts/PindahLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PindahLevel : MonoBehaviour
+{
+    public int nextSceneLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+     nextSceneLoad = SceneManager.GetActiveScene().buildIndex +1;   
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D(Collider2D coll){
+        if(coll.gameObject.tag == "Player"){
+            SceneManager.LoadScene(nextSceneLoad);
+            if(nextSceneLoad >PlayerPrefs.GetInt("levelAt")){
+                PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TheWin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheWin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D (Collider2D coll){
+        if(coll.gameObject.tag== "Player"){
+            SceneManager.LoadScene("winn");
+        }
+    }
+    
+}
